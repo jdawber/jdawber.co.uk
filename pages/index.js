ae75a310569567e8a49259c8bc042447650d62e8
Jonathan Dawber <jonathan.dawber@icloud.com>
2021-03-27 18:55:01 +0000

    Site update

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,91 @@
-import Head from 'next/head'
+import Head from "next/head";
+
+import LinkedIn from "../icons/linkedin.svg";
+import GitHub from "../icons/github.svg";
+import Facebook from "../icons/facebook.svg";
+import Instagram from "../icons/instagram.svg";
 
 export default function Home() {
-  return (
-	  <>
-		<Head>
-			<title>jdawber.co.uk</title>
-			<meta name="description" content="Jonathan Dawber's personal website" />
-			<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=1" />
-			<meta name="google-site-verification" content="c4Xf-Mdm2ka3EEruESb1i7n39zzbG-R1tlXWzWzXB7c" />
-			<link rel="dns-prefetch" href="https://www.googletagmanager.com"/>
-			<link rel="preload" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" as="style" type="text/css"/>
-		</Head>
-		<main>
-			<h1>Jonathan Dawber</h1>
-			<p>I live in Manchester.</p>
-			<p>I'm married, have one son and have two cats.</p>
-			<p>I write code for <a href="https://www.weaveability.com">Weaveability</a>.</p>
-			<p>I write <a href="https://github.com/jdawber">code</a> in my spare time too.</p>
-			<p>I support Manchester United.</p>
-			<p>I like cars.</p>
-			<p>I will be adding more stuff to my site, eventually.</p>
-		</main>
-	</>
-  )
+	return (
+		<>
+			<Head>
+				<title>
+					Jonathan Dawber | Full-stack developer, architect, leader,
+					mentor.
+				</title>
+				<meta
+					name="description"
+					content="Jonathan Dawber's personal website. Full-stack developer, architect, leader, mentor."
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, user-scalable=1"
+				/>
+				<meta
+					name="google-site-verification"
+					content="c4Xf-Mdm2ka3EEruESb1i7n39zzbG-R1tlXWzWzXB7c"
+				/>
+				<link
+					rel="dns-prefetch"
+					href="https://www.googletagmanager.com"
+				/>
+			</Head>
+			<main>
+				<section id="introduction">
+					<p>
+						Hi, I'm <span class="highlight">Jonathan Dawber</span>.
+						<br />
+						Full-stack developer, architect, leader, mentor.
+					</p>
+				</section>
+				<section id="about">
+					<h1>About Me</h1>
+					<ol>
+						<li>HTML</li>
+						<li>CSS</li>
+						<li>JavaScript</li>
+						<li>React</li>
+						<li>Node</li>
+						<li>Java</li>
+						<li>SAP</li>
+						<li>ABAP</li>
+						<li>SAPUI5</li>
+						<li>Amazon Web Services</li>
+						<li>Google Cloud Platform</li>
+						<li>SAP Business Technology Platform</li>
+						<li>Certified Jira Administrator</li>
+						<li>DevSecOps</li>
+						<li>CI/CD</li>
+						<li>Infrastructure as Code</li>
+						<li>Terraform</li>
+						<li>DevSecOps</li>
+					</ol>
+				</section>
+				<footer>
+					<ul>
+						<li>
+							<a href="https://www.linkedin.com/in/jdawber/">
+								<LinkedIn class="icon" />
+							</a>
+						</li>
+						<li>
+							<a href="https://github.com/jdawber/">
+								<GitHub class="icon" />
+							</a>
+						</li>
+						<li>
+							<a href="https://www.facebook.com/jdawber/">
+								<Facebook class="icon" />
+							</a>
+						</li>
+						<li>
+							<a href="https://www.instagram.com/jdawber85/">
+								<Instagram class="icon" />
+							</a>
+						</li>
+					</ul>
+				</footer>
+			</main>
+		</>
+	);
 }
